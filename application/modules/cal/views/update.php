9be7e34a5427afec3eacc54d3ee7aeb338686562
khecaqs <khecaqs@gmail.com>
2017-04-26 23:38:18 +0800

    add update.php

---
+++ application/modules/cal/views/update.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<!DOCTYPE>
+<html>
+<head>
+	<title>My Calendar</title>
+	<meta charset="UTF-8">
+
+    <link href="<?php echo base_url("assets/css/bootstrap.css"); ?>" rel="stylesheet" type="text/css" />
+    <!-- link jquery ui css-->
+    <link href="<?php echo base_url('assets/js/jquery-ui-1.11.4/jquery-ui.min.css'); ?>" rel="stylesheet" type="text/css" />
+    <!--include jquery library-->
+    <script src="<?php echo base_url('assets/js/jquery-1.12.0.min.js'); ?>"></script>
+    <!--load jquery ui js file-->
+    <script src="<?php echo base_url('assets/js/jquery-ui-1.11.4/jquery-ui.min.js'); ?>"></script>
+
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="<?php echo base_url("assets/js/jquery-ui-1.11.4/jquery-ui.css"); ?>">
+	
+	<style>
+	
+	</style>
+	
+</head>
+<body>
+
+
+</body>
+</html>
